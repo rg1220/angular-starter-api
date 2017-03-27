eda1a1d07440cf74ba2e7b33b3890b280a1f1e5e
Randy Groff <randy@randygroff.com>
2017-03-27 09:29:13 -0400

    Mini people API

---
+++ routes/people/index.js
@@ -0,0 +1,54 @@
+var router = require('express').Router();
+
+//TODO: Move this into a database
+var people = [];
+
+router.get('/', function(req, res) {
+  res.json({
+    people: people
+  });
+});
+
+router.post('/', function(req, res) {
+  var id = Math.random() * 100000;
+  var data = Object.assign({}, req.body, {id: id});
+  people.push(data);
+
+  res.json({
+    person: data
+  });
+});
+
+router.get('/:id', function(req, res) {
+  res.json({
+    person: people.find(function(person) {
+      return (person.id === req.params.id);
+    })
+  });
+});
+
+router.put('/:id', function(req, res) {
+  var data = Object.assign({}, req.body);
+  var index = people.findIndex(function(person) {
+    return (person.id === req.params.id);
+  });
+  if (index === -1) {
+    res.json({});
+  } else {
+    people.splice(index, 1, data);
+
+    res.json({
+      person: data
+    });
+  }
+});
+
+router.delete('/:id', function(req, res) {
+  res.json({
+    person: people.filter(function(person) {
+      return (person.id !== req.params.id);
+    })
+  });
+});
+
+module.exports = router;
